8b7efa66307851205d2b22dfa9cde03601d9e9c0
Hitha <hithabadikillaya@gmail.com>
2025-12-30 00:59:38 +0530

    chore: add tailwind css setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./index.css";
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>YOOOOOOOOOO</h1>
+    <div className="min-h-screen bg-background text-foreground flex items-center justify-center">
+      <h1 className="text-7xl font-bold text-primary">
+        D.A.S.H
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
